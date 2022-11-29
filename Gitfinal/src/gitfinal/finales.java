17d0e30b8617aa0b9e07b6cec09b3c31ed657eeb
ptorres8870 <ptorres8870@ceuandalucia.es>
2022-11-29 17:14:27 +0100

    syso

---
+++ Gitfinal/src/gitfinal/finales.java
@@ -0,0 +1,7 @@
+package gitfinal;
+
+public class finales {
+public static void main(String[] args) {
+	System.out.println("khegahg");
+}
+}
---
+++ Gitfinal/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TECH
+ *
+ */
+module Gitfinal {
+}
